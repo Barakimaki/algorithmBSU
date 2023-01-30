adf0c46c9f1af0918e5bd90b84ed8f71b87d043e
Adamonis Arseni <arsenaloo97@gmail.com>
2023-01-30 17:30:45 +0300

    lab 5 update

--- LAB5/lab.js
+++ LAB5/lab.js
@@ -1,43 +1,33 @@
 class Genetic {
     constructor() {
-        this.n = Math.floor(Math.random() * 100)
+        this.n = 50
         this.population = []
         this.winners = []
         this.generation = 0
         for (let i = 0; i < this.n; i++) {
             let individual = []
-            for (let j = 0; j < 50; j++) {
-               // let bit = Math.random()
-               // bit >= 0.5 ? individual.push(1) : individual.push(0)
-                individual.push(0)
+            for (let j = 0; j < 5; j++) {
+                let bit = Math.floor(Math.random() * 200 - 100)
+                individual.push(bit)
             }
-            this.population.push({individual, fitness: individual.reduce((a, c) => a + c, 0)})
-        }
-        this.averageNumber = this.avg()
-    }
+            this.population.push({
+                individual,
+                answer: this.answer(individual),
+                fitness: this.fitness(individual),
 
-    avg() {
-        let avg = 0
-        for (let i = 0; i < this.n; i++) {
-            avg += this.population[i].fitness;
+            })
         }
-        return Math.floor(avg / this.n)
     }
 
     getPopulation() {
         console.log('Generation ' + this.generation)
         console.log(this.population)
-        console.log('Average number ' + this.averageNumber)
     }
 
     selection() {
         this.shufflePopulation()
         for (let i = 0; i < this.n; i++) {
-            if (i === this.n - 1) {
-                this.winners.push(this.population[i])
-                break;
-            }
-            let winner = this.population[i].fitness >= this.population[i + 1].fitness
+            let winner = Math.abs(this.population[i].fitness) <= Math.abs(this.population[i + 1].fitness)
                 ? this.population[i]
                 : this.population[i + 1]
             this.winners.push(winner)
@@ -48,19 +38,22 @@ class Genetic {
 
     crossing() {
         let newGeneration = []
-        for (let i = 0; i < this.winners.length-1; i++) {
+        for (let i = 0; i < this.winners.length; i++) {
             for (let j = 0; j < 2; j++) {
                 let firstParent = this.winners[i].individual
-                let random = Math.floor(Math.random() * this.winners.length)
+                let random = Math.floor(Math.random() * (this.winners.length-1))
                 if (random === i || random === this.winners.length) {
-                    random > this.winners.length ? random-- : random++
+                    random > this.winners.length-1 ? random-- : random++
+                }
+               // console.log(this.winners[random].individual)
+                if(this.winners[random] === undefined){
+                    debugger
                 }
                 let secondParent = this.winners[random].individual
                 let descendant1 = {individual: []}
                 let descendant2 = {individual: []}
-                for (let k = 0; k < 50; k++) {
-                    let divider = Math.floor(Math.random()*50)
-                    if (k < divider) {
+                for (let k = 0; k < 5; k++) {
+                    if (k < 3) {
                         descendant1.individual[k] = firstParent[k]
                         descendant2.individual[k] = secondParent[k]
                     } else {
@@ -68,14 +61,25 @@ class Genetic {
                         descendant1.individual[k] = secondParent[k]
                     }
                 }
-                descendant1.fitness = descendant1.individual.reduce((a, c) => a + c, 0)
-                descendant2.fitness = descendant2.individual.reduce((a, c) => a + c, 0)
+                for (let k = 0; k < 5; k++) {
+                    if(k < 2 || k> 4){
+                        descendant1.individual[k] = firstParent[k]
+                        descendant2.individual[k] = secondParent[k]
+                    } else {
+                        descendant2.individual[k] = firstParent[k]
+                        descendant1.individual[k] = secondParent[k]
+                    }
+                }
+                descendant1.fitness = this.fitness(descendant1.individual)
+                descendant2.fitness = this.fitness(descendant2.individual)
+                descendant1.answer = this.answer(descendant1.individual)
+                descendant2.answer = this.answer(descendant2.individual)
                 newGeneration.push(descendant1)
                 newGeneration.push(descendant2)
             }
         }
         this.winners = []
-        newGeneration.sort((a, b) => b.fitness - a.fitness)
+        newGeneration.sort((a, b) => Math.abs(a.fitness) - Math.abs(b.fitness))
         this.population = []
         for (let i = 0; i < this.n; i++) {
             this.population.push(newGeneration[i])
@@ -86,18 +90,21 @@ class Genetic {
     mutation() {
         this.shufflePopulation()
         for (let i = 0; i < this.n; i++) {
-            for (let j = 0; j < 50; j++) {
+            for (let j = 0; j < 5; j++) {
                 let isMutation = Math.random()
                 if (isMutation < 0.1) {
-                    this.population[i].individual[j] === 0
-                        ? this.population[i].individual[j] = 1
-                        : this.population[i].individual[j] = 0
+                    this.population[i].individual[j] = Math.floor(Math.random()*200 - 100)
                 }
             }
         }
-        this.averageNumber = this.avg()
+        let flag = 0
+        for(let i = 0 ; i< this.n; i++){
+            if(this.population[i].fitness === 0){
+                flag +=1
+            }
+        }
         this.generation = this.generation + 1
-        if (this.generation === 50 || this.averageNumber >= 40) {
+        if (this.generation === 500 || flag === 50) {
             console.log('Final')
             this.getPopulation()
         } else {
@@ -116,7 +123,34 @@ class Genetic {
         }
     }
 
+    fitness(individual) {
+        // let equales1 = [[1,0,2,2,2], [0,0,0,1,0], [0,0,1,0,2], [0,0,1,2,2], [1,2,2,1,2]]
+        // let solve1 = -50
+        // let answer1 = 0
+        // for(let i = 0; i < 5; i++){
+        //     let part1 = 1
+        //     for(let j = 0; j < 5; j++){
+        //         part1*= individual[j]**equales1[i][j]
+        //     }
+        //     answer1+=part1
+        // }
+        let solve1 = -50
+        let answer1 = this.answer(individual)
+        return answer1-solve1
+    }
 
+    answer(individual){
+        let equales1 = [[1,0,2,2,2], [0,0,0,1,0], [0,0,1,0,2], [0,0,1,2,2], [1,2,2,1,2]]
+        let answer1 = 0
+        for(let i = 0; i < 5; i++){
+            let part1 = 1
+            for(let j = 0; j < 5; j++){
+                part1*= individual[j]**equales1[i][j]
+            }
+            answer1+=part1
+        }
+        return answer1
+    }
 }
 
 let genetic = new Genetic();
